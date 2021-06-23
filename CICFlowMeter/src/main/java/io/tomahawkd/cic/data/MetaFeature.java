34fb422abbc6f8137266fd5334f9ac508621758f
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2021-06-23 22:33:56 +0800

    remove unused delegate

--- CICFlowMeter/src/main/java/io/tomahawkd/cic/data/MetaFeature.java
+++ CICFlowMeter/src/main/java/io/tomahawkd/cic/data/MetaFeature.java
@@ -3,7 +3,7 @@ package io.tomahawkd.cic.data;
 public enum MetaFeature implements PackageFeature {
 
     // Protocols
-    IPV4, IPV6, L2TP, TCP, UDP, HTTP,
+    IPV4, IPV6, TCP, UDP, HTTP,
 
     // Src and Dst information
     SRC, DST, SRC_PORT, DST_PORT, PROTO, PAYLOAD_LEN, HEADER_LEN

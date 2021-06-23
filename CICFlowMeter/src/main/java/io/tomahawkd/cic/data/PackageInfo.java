484e7032144e93ef35b32906b6a39108383720c1
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2021-06-23 22:34:23 +0800

    remove some check to speed up the procedure

--- CICFlowMeter/src/main/java/io/tomahawkd/cic/data/PackageInfo.java
+++ CICFlowMeter/src/main/java/io/tomahawkd/cic/data/PackageInfo.java
@@ -1,6 +1,5 @@
 package io.tomahawkd.cic.data;
 
-import com.google.common.primitives.Primitives;
 import org.jnetpcap.packet.format.FormatUtils;
 
 import java.util.Arrays;
@@ -14,6 +13,15 @@ public class PackageInfo {
     private final Map<PackageFeature, Object> data;
     private long timestamp;
     private int payloadPacket = 0;
+
+    // Cached Meta-data of the package
+    private byte[] src;
+    private byte[] dst;
+    private int srcPort;
+    private int dstPort;
+    private int protocol;
+    private int payloadLen;
+    private int headerLen;
 
     public PackageInfo(long id) {
         data = new HashMap<>();
@@ -31,15 +39,7 @@ public class PackageInfo {
     @SuppressWarnings("all")
     public <T> T getFeature(PackageFeature feature, Class<T> type) {
         Object o = data.get(feature);
-        if (o == null) return null;
-        if (type.isPrimitive()) {
-            type = Primitives.wrap(type);
-        }
-        if (type.isAssignableFrom(o.getClass())) {
-            return (T) o;
-        } else {
-            throw new RuntimeException("The expecting type " + type + " is not match with the object " + o.getClass());
-        }
+        return (T) o;
     }
 
     @SuppressWarnings("all")
@@ -53,27 +53,30 @@ public class PackageInfo {
         }
     }
 
-    public <T> boolean hasFeature(PackageFeature feature, Class<T> type) {
-        if (!data.containsKey(feature)) return false;
-        if (type.isPrimitive()) {
-            type = Primitives.wrap(type);
-        }
-        return type.isAssignableFrom(data.get(feature).getClass());
+    public boolean hasFeature(PackageFeature feature) {
+        return data.containsKey(feature);
     }
 
     public void finishParse() {
         // check compulsory elements
-        if ((hasFeature(MetaFeature.IPV4, boolean.class) || hasFeature(MetaFeature.IPV6, boolean.class)) &&
-                (hasFeature(MetaFeature.TCP, boolean.class) || hasFeature(MetaFeature.UDP, boolean.class)) &&
+        if ((hasFeature(MetaFeature.IPV4) || hasFeature(MetaFeature.IPV6)) &&
+                (hasFeature(MetaFeature.TCP) || hasFeature(MetaFeature.UDP)) &&
                 //hasFeature(MetaFeature.HTTP, boolean.class) &&
-                hasFeature(MetaFeature.SRC, byte[].class) &&
-                hasFeature(MetaFeature.DST, byte[].class) &&
-                hasFeature(MetaFeature.SRC_PORT, int.class) &&
-                hasFeature(MetaFeature.DST_PORT, int.class) &&
-                hasFeature(MetaFeature.PROTO, int.class) &&
-                hasFeature(MetaFeature.HEADER_LEN, int.class) &&
-                hasFeature(MetaFeature.PAYLOAD_LEN, int.class)) {
+                hasFeature(MetaFeature.SRC) &&
+                hasFeature(MetaFeature.DST) &&
+                hasFeature(MetaFeature.SRC_PORT) &&
+                hasFeature(MetaFeature.DST_PORT) &&
+                hasFeature(MetaFeature.PROTO) &&
+                hasFeature(MetaFeature.HEADER_LEN) &&
+                hasFeature(MetaFeature.PAYLOAD_LEN)) {
 
+            this.src = getFeature(MetaFeature.SRC, byte[].class);
+            this.dst = getFeature(MetaFeature.DST, byte[].class);
+            this.srcPort = getFeature(MetaFeature.SRC_PORT, int.class);
+            this.dstPort = getFeature(MetaFeature.DST_PORT, int.class);
+            this.protocol = getFeature(MetaFeature.PROTO, int.class);
+            this.payloadLen = getFeature(MetaFeature.PAYLOAD_LEN, int.class);
+            this.headerLen = getFeature(MetaFeature.HEADER_LEN, int.class);
             generateFlowId();
         } else {
             throw new RuntimeException("Package is not complete.");
@@ -82,49 +85,42 @@ public class PackageInfo {
 
     private void generateFlowId() {
         boolean forward = true;
-        byte[] src = getFeature(MetaFeature.SRC, byte[].class);
-        byte[] dst = getFeature(MetaFeature.DST, byte[].class);
-        int srcPort = getFeature(MetaFeature.SRC_PORT, int.class);
-        int dstPort = getFeature(MetaFeature.DST_PORT, int.class);
-        int protocol = getFeature(MetaFeature.PROTO, int.class);
 
-        for (int i = 0; i < src.length; i++) {
-            if (((Byte) (src[i])).intValue() != ((Byte) (dst[i])).intValue()) {
-                if (((Byte) (src[i])).intValue() > ((Byte) (dst[i])).intValue()) {
+        for (int i = 0; i < this.src.length; i++) {
+            if (((Byte) (this.src[i])).intValue() != ((Byte) (this.dst[i])).intValue()) {
+                if (((Byte) (this.src[i])).intValue() > ((Byte) (this.dst[i])).intValue()) {
                     forward = false;
                 }
-                i = src.length;
+                i = this.src.length;
             }
         }
 
         if (forward) {
-            this.flowId = this.getSourceIP() + "-" + this.getDestinationIP() + "-" + srcPort + "-" + dstPort + "-" + protocol;
+            this.flowId = this.getSourceIP() + "-" + this.getDestinationIP() + "-" +
+                    this.srcPort + "-" + this.dstPort + "-" + this.protocol;
         } else {
-            this.flowId = this.getDestinationIP() + "-" + this.getSourceIP() + "-" + dstPort + "-" + srcPort + "-" + protocol;
+            this.flowId = this.getDestinationIP() + "-" + this.getSourceIP() + "-" +
+                    this.dstPort + "-" + this.srcPort + "-" + this.protocol;
         }
     }
 
     public String getSourceIP() {
-        return FormatUtils.ip(getFeature(MetaFeature.SRC, byte[].class));
+        return FormatUtils.ip(this.src);
     }
 
     public String getDestinationIP() {
-        return FormatUtils.ip(getFeature(MetaFeature.DST, byte[].class));
+        return FormatUtils.ip(this.dst);
     }
 
     public String fwdFlowId() {
-        int srcPort = getFeature(MetaFeature.SRC_PORT, int.class);
-        int dstPort = getFeature(MetaFeature.DST_PORT, int.class);
-        int protocol = getFeature(MetaFeature.PROTO, int.class);
-        this.flowId = this.getSourceIP() + "-" + this.getDestinationIP() + "-" + srcPort + "-" + dstPort + "-" + protocol;
+        this.flowId = this.getSourceIP() + "-" + this.getDestinationIP() + "-" +
+                this.srcPort + "-" + this.dstPort + "-" + this.protocol;
         return this.flowId;
     }
 
     public String bwdFlowId() {
-        int srcPort = getFeature(MetaFeature.SRC_PORT, int.class);
-        int dstPort = getFeature(MetaFeature.DST_PORT, int.class);
-        int protocol = getFeature(MetaFeature.PROTO, int.class);
-        this.flowId = this.getDestinationIP() + "-" + this.getSourceIP() + "-" + dstPort + "-" + srcPort + "-" + protocol;
+        this.flowId = this.getDestinationIP() + "-" + this.getSourceIP() + "-" +
+                this.dstPort + "-" + this.srcPort + "-" + this.protocol;
         return this.flowId;
     }
 
@@ -145,37 +141,35 @@ public class PackageInfo {
     }
 
     public byte[] getSrc() {
-        byte[] src = getFeature(MetaFeature.SRC, byte[].class);
-        return Arrays.copyOf(src, src.length);
+        return Arrays.copyOf(this.src, this.src.length);
     }
 
     public byte[] getDst() {
-        byte[] dst = getFeature(MetaFeature.DST, byte[].class);
-        return Arrays.copyOf(dst, dst.length);
+        return Arrays.copyOf(this.dst, this.dst.length);
     }
 
     public int getSrcPort() {
-        return getFeature(MetaFeature.SRC_PORT, int.class);
+        return this.srcPort;
     }
 
     public int getDstPort() {
-        return getFeature(MetaFeature.DST_PORT, int.class);
+        return this.dstPort;
     }
 
     public int getProtocol() {
-        return getFeature(MetaFeature.PROTO, int.class);
+        return this.protocol;
     }
 
     public boolean isForwardPacket(byte[] sourceIP) {
-        return Arrays.equals(sourceIP, getFeature(MetaFeature.SRC, byte[].class));
+        return Arrays.equals(sourceIP, this.src);
     }
 
     public long getPayloadBytes() {
-        return getFeature(MetaFeature.PAYLOAD_LEN, int.class);
+        return this.payloadLen;
     }
 
     public long getHeaderBytes() {
-        return getFeature(MetaFeature.HEADER_LEN, int.class);
+        return this.headerLen;
     }
 
     public int getPayloadPacket() {

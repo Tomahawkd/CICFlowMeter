6ec8a4515da5d0fa542a08d8f716c18c70932e9f
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2021-07-06 15:25:39 +0800

    according to the original repo, the subflow algorithm get clearer

--- CICFlowMeter/src/main/java/io/tomahawkd/cic/flow/features/FlowActiveFeature.java
+++ CICFlowMeter/src/main/java/io/tomahawkd/cic/flow/features/FlowActiveFeature.java
@@ -14,10 +14,6 @@ import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
         FlowFeatureTag.idl_std,
         FlowFeatureTag.idl_max,
         FlowFeatureTag.idl_min,
-        FlowFeatureTag.subfl_fw_pkt,
-        FlowFeatureTag.subfl_fw_byt,
-        FlowFeatureTag.subfl_bw_pkt,
-        FlowFeatureTag.subfl_bw_byt,
 })
 public class FlowActiveFeature extends AbstractFlowFeature {
 
@@ -27,19 +23,13 @@ public class FlowActiveFeature extends AbstractFlowFeature {
     private final SummaryStatistics flowActive = new SummaryStatistics();
     private final SummaryStatistics flowIdle = new SummaryStatistics();
 
-    private long sfLastPacketTS=-1;
-    private int sfCount=0;
-    private long sfAcHelper=-1;
-
     public FlowActiveFeature(Flow flow) {
         super(flow);
     }
 
     @Override
     public void addPacket(PacketInfo info, boolean fwd) {
-        long timeout = getBasicInfo().getFlowActivityTimeOut();
-        updateActiveIdleTime(info.getTimestamp(), timeout);
-        detectUpdateSubflows(info, timeout);
+        updateActiveIdleTime(info.getTimestamp(), getBasicInfo().getFlowActivityTimeOut());
     }
 
     public void updateActiveIdleTime(long currentTime, long threshold){
@@ -55,49 +45,11 @@ public class FlowActiveFeature extends AbstractFlowFeature {
         }
     }
 
-    public double getSflow_fbytes(){
-        if(sfCount <= 0) return 0;
-        return getDep(PacketSizeFeature.class).getForwardPacketBytes()/sfCount;
-    }
-
-    public long getSflow_fpackets(){
-        if(sfCount <= 0) return 0;
-        return getDep(PacketSizeFeature.class).getForwardPacketCount()/sfCount;
-    }
-
-    public double getSflow_bbytes(){
-        if(sfCount <= 0) return 0;
-        return getDep(PacketSizeFeature.class).getBackwardPacketBytes()/sfCount;
-    }
-    public long getSflow_bpackets(){
-        if(sfCount <= 0) return 0;
-        return getDep(PacketSizeFeature.class).getBackwardPacketCount()/sfCount;
-    }
-
-    private void detectUpdateSubflows(PacketInfo packet, long timeout) {
-        if(sfLastPacketTS == -1){
-            sfLastPacketTS = packet.getTimestamp();
-            sfAcHelper   = packet.getTimestamp();
-        }
-
-        if(((packet.getTimestamp() - sfLastPacketTS)/(double)1000000)  > 1.0){
-            sfCount ++ ;
-            long lastSFduration = packet.getTimestamp() - sfAcHelper;
-            updateActiveIdleTime(packet.getTimestamp(), timeout);
-            sfAcHelper = packet.getTimestamp();
-        }
-        sfLastPacketTS = packet.getTimestamp();
-    }
-
     @Override
     public String exportData() {
         StringBuilder builder = new StringBuilder();
         dataToString(flowActive, builder);
         dataToString(flowIdle, builder);
-        builder.append(getSflow_fpackets()).append(SEPARATOR); // FlowFeatureTag.subfl_fw_pkt,
-        builder.append(getSflow_fbytes()).append(SEPARATOR); // FlowFeatureTag.subfl_fw_byt,
-        builder.append(getSflow_bpackets()).append(SEPARATOR); // FlowFeatureTag.subfl_bw_pkt,
-        builder.append(getSflow_bbytes()).append(SEPARATOR); // FlowFeatureTag.subfl_bw_byt,
         return builder.toString();
     }
 

5f5dfecaf962fc78e1c526bae15da9bc4ceeebee
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2021-07-07 20:21:20 +0800

    add features

--- CICFlowMeter/src/main/java/io/tomahawkd/cic/data/HttpPacketDelegate.java
+++ CICFlowMeter/src/main/java/io/tomahawkd/cic/data/HttpPacketDelegate.java
@@ -24,11 +24,13 @@ public class HttpPacketDelegate extends AbstractPacketDelegate {
             } catch (NumberFormatException e) {
                 dst.addFeature(Feature.CONTENT_LEN, 0);
             }
+            dst.addFeature(Feature.METHOD, http.fieldValue(Http.Request.RequestMethod));
             dst.addFeature(Feature.UA, http.fieldValue(Http.Request.User_Agent));
             dst.addFeature(Feature.CONNECTION, http.fieldValue(Http.Request.Connection));
             dst.addFeature(Feature.CACHE, http.fieldValue(Http.Request.Cache_Control));
             dst.addFeature(Feature.URL, http.fieldValue(Http.Request.RequestUrl));
             dst.addFeature(Feature.CHARSET, http.fieldValue(Http.Request.Accept_Charset));
+            dst.addFeature(Feature.REFERER, http.fieldValue(Http.Request.Referer));
 
         } else {
             dst.addFeature(Feature.REQUEST, false);
@@ -49,6 +51,7 @@ public class HttpPacketDelegate extends AbstractPacketDelegate {
 
         // Request
         UA(String.class), CONNECTION(String.class), CACHE(String.class), URL(String.class), CHARSET(String.class),
+        REFERER(String.class), METHOD(String.class),
 
         // Response
         STATUS(Integer.class), CONTENT_TYPE(String.class);

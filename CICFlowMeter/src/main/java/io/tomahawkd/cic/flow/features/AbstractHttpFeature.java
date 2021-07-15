98c01a13d2df0648f83a9b468774f3aac7093121
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2021-07-15 18:18:32 +0800

    add abstract http layer class for request and response packets

---
+++ CICFlowMeter/src/main/java/io/tomahawkd/cic/flow/features/AbstractHttpFeature.java
@@ -0,0 +1,43 @@
+package io.tomahawkd.cic.flow.features;
+
+import io.tomahawkd.cic.flow.Flow;
+import io.tomahawkd.cic.packet.HttpPacketDelegate;
+import io.tomahawkd.cic.packet.PacketInfo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public abstract class AbstractHttpFeature extends AbstractFlowFeature {
+
+    private static final Logger logger = LogManager.getLogger(AbstractHttpFeature.class);
+
+    public AbstractHttpFeature(Flow flow) {
+        super(flow);
+    }
+
+    @Override
+    public final void addPacket(PacketInfo info, boolean fwd) {
+
+        Boolean request = info.getFeature(HttpPacketDelegate.Feature.REQUEST, Boolean.class);
+        if (request == null) {
+            logger.warn("Packet {} has no request tag, discarded", info.getFlowId());
+            logger.warn("Packet Content: {}", info.toString());
+            return;
+        }
+
+        addGenericPacket(info, request);
+        if (request) addRequestPacket(info);
+        else addResponsePacket(info);
+    }
+
+    public void addGenericPacket(PacketInfo info, boolean isRequest) {
+
+    }
+
+    public void addRequestPacket(PacketInfo info) {
+
+    }
+
+    public void addResponsePacket(PacketInfo info) {
+
+    }
+}

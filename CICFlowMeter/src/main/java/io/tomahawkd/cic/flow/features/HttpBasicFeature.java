6e194f8c1e08856fd8935617d91d390f4b54cf94
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2021-07-15 18:19:31 +0800

    rename class and feature

--- CICFlowMeter/src/main/java/io/tomahawkd/cic/flow/features/HttpBasicFeature.java
+++ CICFlowMeter/src/main/java/io/tomahawkd/cic/flow/features/HttpBasicFeature.java
@@ -1,10 +1,11 @@
 package io.tomahawkd.cic.flow.features;
 
+import io.tomahawkd.cic.flow.Flow;
 import io.tomahawkd.cic.packet.HttpPacketDelegate;
 import io.tomahawkd.cic.packet.PacketInfo;
-import io.tomahawkd.cic.flow.Flow;
-import nl.basjes.parse.useragent.UserAgent;
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Optional;
 
@@ -14,22 +15,23 @@ import java.util.Optional;
         FlowFeatureTag.content_length_max,
         FlowFeatureTag.content_length_min,
         FlowFeatureTag.content_length_total,
-        FlowFeatureTag.fw_content_length_avg,
-        FlowFeatureTag.fw_content_length_std,
-        FlowFeatureTag.fw_content_length_max,
-        FlowFeatureTag.fw_content_length_min,
-        FlowFeatureTag.fw_content_length_total,
-        FlowFeatureTag.bw_content_length_avg,
-        FlowFeatureTag.bw_content_length_std,
-        FlowFeatureTag.bw_content_length_max,
-        FlowFeatureTag.bw_content_length_min,
-        FlowFeatureTag.bw_content_length_total,
+        FlowFeatureTag.req_content_length_avg,
+        FlowFeatureTag.req_content_length_std,
+        FlowFeatureTag.req_content_length_max,
+        FlowFeatureTag.req_content_length_min,
+        FlowFeatureTag.req_content_length_total,
+        FlowFeatureTag.res_content_length_avg,
+        FlowFeatureTag.res_content_length_std,
+        FlowFeatureTag.res_content_length_max,
+        FlowFeatureTag.res_content_length_min,
+        FlowFeatureTag.res_content_length_total,
         FlowFeatureTag.keep_alive_packet_ratio,
         FlowFeatureTag.method_get_count,
         FlowFeatureTag.method_post_count,
-        FlowFeatureTag.referer_count,
 })
-public class HttpFeature extends AbstractFlowFeature {
+public class HttpBasicFeature extends AbstractHttpFeature {
+
+    private static final Logger logger = LogManager.getLogger(HttpBasicFeature.class);
 
     private final SummaryStatistics content_length = new SummaryStatistics();
     private final SummaryStatistics content_length_req = new SummaryStatistics();
@@ -37,57 +39,45 @@ public class HttpFeature extends AbstractFlowFeature {
     private long keepAliveCount = 0L;
     private long getCount = 0;
     private long postCount = 0;
-    private long refererCount = 0;
 
-    public HttpFeature(Flow flow) {
+    public HttpBasicFeature(Flow flow) {
         super(flow);
     }
 
     @Override
-    public void addPacket(PacketInfo info, boolean fwd) {
-
-        Boolean request = info.getFeature(HttpPacketDelegate.Feature.REQUEST, Boolean.class);
-        if (request == null) return;
-
-
+    public void addGenericPacket(PacketInfo info, boolean isRequest) {
         int contentLength = Optional.ofNullable(
                 info.getFeature(HttpPacketDelegate.Feature.CONTENT_LEN, Integer.class))
                 .orElse(0);
         content_length.addValue(contentLength);
 
-        // we only care about the request
-        if (request) {
+        if (isRequest) {
             content_length_req.addValue(contentLength);
-
-            // TODO: URL query and more
-            String url = info.getFeature(HttpPacketDelegate.Feature.URL, String.class);
-
-            String connection = info.getFeature(HttpPacketDelegate.Feature.CONNECTION, String.class);
-            if (connection != null) {
-                if (connection.equalsIgnoreCase("keep-alive")) keepAliveCount++;
-            }
-
-            String method = info.getFeature(HttpPacketDelegate.Feature.METHOD, String.class);
-            if (method != null) {
-                if (method.equalsIgnoreCase("get")) getCount++;
-                else if (method.equalsIgnoreCase("post")) postCount++;
-            }
-
-            String referer = info.getFeature(HttpPacketDelegate.Feature.REFERER, String.class);
-            if (referer != null) {
-                refererCount++;
-                // TODO: Need more check such as from a related link/search engine/totally irrelevant
-            }
-
-            // TODO check ua
-            UserAgent ua = info.getFeature(HttpPacketDelegate.Feature.UA, UserAgent.class);
-
-            // TODO: check accept headers
-            String accept = info.getFeature(HttpPacketDelegate.Feature.CONTENT_TYPE, String.class);
-            String encoding = info.getFeature(HttpPacketDelegate.Feature.ENCODING, String.class);
-            String lang = info.getFeature(HttpPacketDelegate.Feature.LANGUAGE, String.class);
         } else {
             content_length_res.addValue(contentLength);
+        }
+    }
+
+    @Override
+    public void addRequestPacket(PacketInfo info) {
+        // TODO: URL query and more
+        String path = info.getFeature(HttpPacketDelegate.Feature.URL, String.class);
+        String host = info.getFeature(HttpPacketDelegate.Feature.HOST, String.class);
+        if (host == null) {
+            logger.warn("Packet {} has no host in HTTP protocol.", info.getFlowId());
+            logger.warn("Packet Content: {}", info.toString());
+
+        }
+
+        String connection = info.getFeature(HttpPacketDelegate.Feature.CONNECTION, String.class);
+        if (connection != null) {
+            if (connection.equalsIgnoreCase("keep-alive")) keepAliveCount++;
+        }
+
+        String method = info.getFeature(HttpPacketDelegate.Feature.METHOD, String.class);
+        if (method != null) {
+            if (method.equalsIgnoreCase("get")) getCount++;
+            else if (method.equalsIgnoreCase("post")) postCount++;
         }
     }
 
@@ -101,7 +91,6 @@ public class HttpFeature extends AbstractFlowFeature {
         builder.append(keepAliveCount).append(SEPARATOR); // FlowFeatureTag.keep_alive_packet_ratio,
         builder.append(getCount).append(SEPARATOR); // FlowFeatureTag.method_get_count,
         builder.append(postCount).append(SEPARATOR); // FlowFeatureTag.method_post_count,
-        builder.append(refererCount).append(SEPARATOR); // FlowFeatureTag.referer_count,
         return builder.toString();
     }
 

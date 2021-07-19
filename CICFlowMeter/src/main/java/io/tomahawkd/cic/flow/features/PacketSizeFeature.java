6434c12a80091fdd4e1950dc334aa92c9f40e504
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2021-07-19 22:52:36 +0800

    avoid NaN

--- CICFlowMeter/src/main/java/io/tomahawkd/cic/flow/features/PacketSizeFeature.java
+++ CICFlowMeter/src/main/java/io/tomahawkd/cic/flow/features/PacketSizeFeature.java
@@ -114,13 +114,21 @@ public class PacketSizeFeature extends AbstractFlowFeature {
             builder.append(flowPacketStats.getMean()).append(SEPARATOR); //                FlowFeatureTag.pkt_len_avg,
             builder.append(flowPacketStats.getStandardDeviation()).append(SEPARATOR); //   FlowFeatureTag.pkt_len_std,
             builder.append(flowPacketStats.getVariance()).append(SEPARATOR); //            FlowFeatureTag.pkt_len_var,
+        } else {
+            addZeroesToBuilder(builder, 5);
         }
 
         subPacketUpdate(builder, fwdPacketStats);
-        builder.append(fwdHeaderStats.getSum()).append(SEPARATOR); //                FlowFeatureTag.fw_hdr_len,
-        builder.append(fwdHeaderStats.getMin()).append(SEPARATOR); // fw_hdr_min
+        if (fwdPacketStats.getN() > 0) {
+            builder.append(fwdHeaderStats.getSum()).append(SEPARATOR); //                FlowFeatureTag.fw_hdr_len,
+            builder.append(fwdHeaderStats.getMin()).append(SEPARATOR); // fw_hdr_min
+        } else addZeroesToBuilder(builder, 2);
+
         subPacketUpdate(builder, bwdPacketStats);
-        builder.append(bwdHeaderStats.getSum()).append(SEPARATOR); //                FlowFeatureTag.bw_hdr_len,
+
+        if (bwdHeaderStats.getN() > 0) {
+            builder.append(bwdHeaderStats.getSum()).append(SEPARATOR); //                FlowFeatureTag.bw_hdr_len,
+        } else builder.append(0).append(SEPARATOR);
 
         if (fwdPacketStats.getN() > 0) {
             builder.append(bwdPacketStats.getN() / fwdPacketStats.getN()).append(SEPARATOR); // down_up_ratio,

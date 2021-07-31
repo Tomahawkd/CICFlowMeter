6a4b5ee523a11f3c166524d0022776e6fbd584d4
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2021-07-31 20:37:08 +0800

    add payload temporarily for app layer parse

--- CICFlowMeter/src/main/java/io/tomahawkd/cic/packet/TcpPacketDelegate.java
+++ CICFlowMeter/src/main/java/io/tomahawkd/cic/packet/TcpPacketDelegate.java
@@ -25,12 +25,13 @@ public class TcpPacketDelegate extends AbstractPacketDelegate {
         dst.addFeature(MetaFeature.HEADER_LEN, tcp.getHeaderLength());
         dst.addFeature(Feature.SEQ, tcp.seq());
         dst.addFeature(Feature.ACK, tcp.ack());
+        dst.addFeature(Feature.PAYLOAD, tcp.getPayload());
         dst.addFeature(MetaFeature.TCP, true);
         return true;
     }
 
     public enum Feature implements PacketFeature {
-        TCP_WINDOW(Integer.class), FLAG(Integer.class), SEQ(Long.class), ACK(Long.class);
+        TCP_WINDOW(Integer.class), FLAG(Integer.class), SEQ(Long.class), ACK(Long.class), PAYLOAD(byte[].class);
 
         private final Class<?> type;
 

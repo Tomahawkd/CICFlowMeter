54ac3ea9cf8b2bb9d30bd9837920e874647c8119
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2021-08-10 02:01:59 +0800

    add to runtime

--- CICFlowMeter/src/main/java/io/tomahawkd/cic/label/factory/LabelFactory.java
+++ CICFlowMeter/src/main/java/io/tomahawkd/cic/label/factory/LabelFactory.java
@@ -1,5 +1,12 @@
 package io.tomahawkd.cic.label.factory;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface LabelFactory {
 
     String name();
